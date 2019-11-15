9348938198b2d6996ecd3aa4f1b32d17196c48da
Fabien LEFEBVRE <contact@d1ceward.com>
2019-11-15 18:36:15 +0100

    Added sheets parsing

---
+++ spec/excel_parser_spec.cr
@@ -0,0 +1,7 @@
+require "./spec_helper"
+
+describe ExcelParser do
+  it "should have a version" do
+    ExcelParser::VERSION.should be_truthy
+  end
+end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,6 @@
 require "spec"
-require "../src/excel-parser"
+require "../src/excel_parser"
+
+def file_io : IO
+  File.new(__DIR__ + "/fixtures/valid.xlsx")
+end
