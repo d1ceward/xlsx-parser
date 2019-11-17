7546cbcd5fa3a08de63b9c31aba715701759e917
Fabien LEFEBVRE <contact@d1ceward.com>
2019-11-17 19:45:22 +0100

    Added shared strings parsing

--- src/excel_parser/book.cr
+++ src/excel_parser/book.cr
@@ -5,24 +5,24 @@ module ExcelParser
   class Book
     getter zip : Zip::File
     getter sheets : Array(Sheet) = [] of Sheet
+    getter shared_strings : Hash(Int32, String)
 
     def initialize(io : IO)
       @zip = Zip::File.new(io)
-    end
+      @shared_strings = {} of Int32 => String
 
-    def sheets
-      @sheets = sheet_rel_nodes.map do |relation|
-        Sheet.new(self, relation["Target"])
+      node = XML.parse(@zip["xl/sharedStrings.xml"].open(&.gets_to_end))
+      node.xpath_nodes("//*[name()='si']/*[name()='t']").each_with_index do |t_node, index|
+        @shared_strings[index] = t_node.content
       end
     end
 
-    # Parse XML node of each relationships corresponding to sheets
-    private def sheet_rel_nodes : XML::NodeSet
-      # Open/parse relationships document
-      xml = XML.parse(@zip["xl/_rels/workbook.xml.rels"].open(&.gets_to_end))
-
-      # Get relationships children as node set
-      xml.xpath_nodes("//*[name()='Relationship' and contains(@Target,'sheet')]")
+    def sheets
+      node = XML.parse(@zip["xl/_rels/workbook.xml.rels"].open(&.gets_to_end))
+      nodes = node.xpath_nodes("//*[name()='Relationship' and contains(@Target,'sheet')]")
+      @sheets = nodes.map do |relation|
+        Sheet.new(self, relation["Target"])
+      end
     end
 
     def close
--- src/excel_parser/sheet.cr
+++ src/excel_parser/sheet.cr
@@ -8,24 +8,3 @@ module ExcelParser
     end
   end
 end
-
-# class Pouet
-#   property pouet : Array(Int32)
-
-#   def initialize(@pouet = pouet)
-#   end
-
-#   def test
-#     _pouet = @pouet.each
-
-#     return Iterator.of do
-#       _pouet.next
-#     end
-#   end
-# end
-
-# pouet = Pouet.new([1, 2, 3, 4])
-
-# pouter = pouet.test
-
-# puts pouter.first(4).flatten
