64997bf37f506f4f99dbdcbf0ded395da5acde6b
Fabien LEFEBVRE <d1ceward@gmail.com>
2019-11-18 14:20:08 +0100

    Added cell parsing

--- spec/excel_parser/sheet_spec.cr
+++ spec/excel_parser/sheet_spec.cr
@@ -3,8 +3,9 @@ require "../spec_helper"
 describe ExcelParser::Sheet do
   book = ExcelParser::Book.new(file_io)
 
-  it "return correct rows number for first sheet" do
-    book.sheets[0].rows
+  it "return correct rows number" do
+    book.sheets[0].rows.size.should eq(3)
+    book.sheets[1].rows.size.should eq(1)
   end
 
   book.close
--- src/excel_parser/sheet.cr
+++ src/excel_parser/sheet.cr
@@ -4,19 +4,33 @@ module ExcelParser
     end
 
     def rows
-      # node = XML::Reader.new(@book.zip["xl/#{@file}"].open(&.gets_to_end))
-      # rows = node.xpath_nodes("//*[name()='row']")
+      node = XML::Reader.new(@book.zip["xl/#{@file}"].open(&.gets_to_end))
+      status = true
 
-      # return Iterator.of do
-      #   row = rows.next
-      #   if rows.is_a?(Iterator::Stop)
-      #     Iterator.stop
-      #   else
-      #     rows.xpath_nodes("//*[name()='v']").map do |v_node|
-      #       @book.shared_strings[v_node.content.to_i]
-      #     end
-      #   end
-      # end
+      return Iterator.of do
+        Iterator.stop
+        row = nil
+        cell = nil
+
+        loop do
+          break unless (status = node.read)
+          if node.name == "row" && node.node_type == XML::Reader::Type::ELEMENT
+            row = {} of String => String
+          elsif node.name == "row" && node.node_type == XML::Reader::Type::END_ELEMENT
+            break
+          elsif node.name == "c" && node.node_type == XML::Reader::Type::ELEMENT
+            cell = node["r"]
+          elsif node.name == "v" && node.node_type == XML::Reader::Type::ELEMENT
+            row.not_nil![cell.not_nil!] = @book.shared_strings[node.read_inner_xml.to_i]
+          end
+        end
+
+        if status
+          row
+        else
+          Iterator.stop
+        end
+      end
     end
   end
 end
