df96d6134018a4e72ce94abe2212f8de9a03a3e7
d1ceward <d1ceward@gmail.com>
2019-11-18 20:08:55 +0100

    Fix folder/file naming for broken require

---
+++ src/xlsx-parser.cr
@@ -0,0 +1,6 @@
+require "./xlsx-parser/book"
+require "./xlsx-parser/sheet"
+
+module XlsxParser
+  VERSION = "0.1.2"
+end
--- src/xlsx_parser.cr
+++
@@ -1,6 +0,0 @@
-require "./xlsx_parser/book"
-require "./xlsx_parser/sheet"
-
-module XlsxParser
-  VERSION = "0.1.1"
-end
