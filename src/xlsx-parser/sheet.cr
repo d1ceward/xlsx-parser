e6c430bfe4c04bc0f9ab66d87570fac0cc9f07a9
Fabien LEFEBVRE <contact@d1ceward.com>
2019-11-19 18:18:57 +0100

    Reduced cyclomatic complexity

--- src/xlsx-parser/sheet.cr
+++ src/xlsx-parser/sheet.cr
@@ -1,40 +1,40 @@
 module XlsxParser
   class Sheet
+    getter node : XML::Reader
+
     def initialize(@book : Book, @file : String)
+      @node = XML::Reader.new(@book.zip["xl/#{@file}"].open(&.gets_to_end))
     end
 
     def rows
-      node = XML::Reader.new(@book.zip["xl/#{@file}"].open(&.gets_to_end))
-      status = true
-
       Iterator.of do
         row = nil
         cell = nil
         cell_type = nil
 
         loop do
-          break unless (status = node.read)
-          if node.name == "row" && node.node_type == XML::Reader::Type::ELEMENT
-            row = {} of String => String | Int32
-          elsif node.name == "row" && node.node_type == XML::Reader::Type::END_ELEMENT
-            break
-          elsif node.name == "c" && node.node_type == XML::Reader::Type::ELEMENT
+          break unless @node.read
+          if @node.name == "row"
+            if @node.node_type == XML::Reader::Type::ELEMENT
+              row = {} of String => String | Int32
+            else
+              break
+            end
+          elsif @node.name == "c" && @node.node_type == XML::Reader::Type::ELEMENT
             cell_type = node["t"]?
             cell = node["r"]?
-          elsif node.name == "v" && node.node_type == XML::Reader::Type::ELEMENT
-            row[cell] = convert(node.read_inner_xml, cell_type) unless row.nil? || cell.nil?
+          elsif @node.name == "v" && @node.node_type == XML::Reader::Type::ELEMENT && row && cell
+            row[cell] = convert(cell_type)
           end
         end
 
-        if status
-          row
-        else
-          Iterator.stop
-        end
+        row || Iterator.stop
       end
     end
 
-    private def convert(value : String, type : String?) : String | Int32
+    private def convert(type : String?) : String | Int32
+      value = @node.read_inner_xml
+
       case type
       when "s"
         @book.shared_strings[value.to_i]
