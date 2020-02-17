848d00539ecb296e3e7d7b89daebfda1973baf78
Fabien LEFEBVRE <d1ceward@gmail.com>
2020-02-17 11:21:22 +0100

    Updated to crystal 0.33.0

--- spec/xlsx-parser/book_spec.cr
+++ spec/xlsx-parser/book_spec.cr
@@ -3,28 +3,40 @@ require "../spec_helper"
 describe XlsxParser::Book do
   describe "#new" do
     it "open an xlsx file successfully" do
-      book = XlsxParser::Book.new(dir_helper("/fixtures/sample.xlsx"))
+      book = XlsxParser::Book.new("./spec/fixtures/sample.xlsx")
       book.should be_truthy
       book.close
     end
 
     it "fail to open a legacy xls file" do
       expect_raises(ArgumentError, "Not a valid file format") do
-        XlsxParser::Book.new(dir_helper("/fixtures/invalid.xls"))
+        XlsxParser::Book.new("./spec/fixtures/invalid.xls")
       end
     end
 
     it "ignore file extensions on request" do
-      path = dir_helper("/fixtures/sample.zip")
-      book = XlsxParser::Book.new(path, check_file_extension: false)
+      book = XlsxParser::Book.new("./spec/fixtures/sample.zip", check_file_extension: false)
       book.close
     end
 
     it "check file extensions on request" do
-      path = dir_helper("/fixtures/sample.zip")
       expect_raises(ArgumentError, "Not a valid file format") do
-        XlsxParser::Book.new(path, check_file_extension: true)
+        XlsxParser::Book.new("./spec/fixtures/sample.zip", check_file_extension: true)
       end
+    end
+  end
+
+  describe "#sheets" do
+    it "return an array of Sheet class instance" do
+      book = XlsxParser::Book.new("./spec/fixtures/sample.xlsx")
+      book.sheets.should be_a(Array(XlsxParser::Sheet))
+      book.close
+    end
+
+    it "return correct number of sheets" do
+      book = XlsxParser::Book.new("./spec/fixtures/sample.xlsx")
+      book.sheets.size.should eq(2)
+      book.close
     end
   end
 end
--- spec/xlsx-parser/sheet_spec.cr
+++ spec/xlsx-parser/sheet_spec.cr
@@ -2,7 +2,7 @@ require "../spec_helper"
 
 describe XlsxParser::Sheet do
   it "return correct rows number" do
-    book = XlsxParser::Book.new(dir_helper("/fixtures/sample.xlsx"))
+    book = XlsxParser::Book.new("./spec/fixtures/sample.xlsx")
     book.sheets[0].rows.size.should eq(4)
     book.sheets[1].rows.size.should eq(1)
     book.close
