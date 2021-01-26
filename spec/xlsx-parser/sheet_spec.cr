153d1c4d376b58ab7cc4a04d70262113f486dc86
d1ceward <d1ceward@gmail.com>
2021-01-26 20:30:58 +0100

    Crystal 0.36.0 update

--- spec/xlsx-parser/sheet_spec.cr
+++ spec/xlsx-parser/sheet_spec.cr
@@ -1,10 +1,29 @@
 require "../spec_helper"
 
 describe XlsxParser::Sheet do
-  it "return correct rows number" do
+  it "return correct rows number for the first sheet" do
     book = XlsxParser::Book.new("./spec/fixtures/sample.xlsx")
     book.sheets[0].rows.size.should eq(4)
+    book.close
+  end
+
+  it "return correct rows number for the second sheet" do
+    book = XlsxParser::Book.new("./spec/fixtures/sample.xlsx")
     book.sheets[1].rows.size.should eq(1)
+    book.close
+  end
+
+  it "return a valid row on the first sheet" do
+    book = XlsxParser::Book.new("./spec/fixtures/sample.xlsx")
+    book.sheets[0].rows.first.should eq({ "A1" => "First Name", "B1" => "Last Name", "C1" => "Email",
+                                          "D1" => "Address", "E1" => "Surprise", "F1" => "Phone",
+                                          "G1" => "Column with int", "H1" => "Empty column" })
+    book.close
+  end
+
+  it "return a valid row on the second sheet" do
+    book = XlsxParser::Book.new("./spec/fixtures/sample.xlsx")
+    book.sheets[1].rows.first.should eq({"A1" => "Not empty…"})
     book.close
   end
 end
--- src/xlsx-parser.cr
+++ src/xlsx-parser.cr
@@ -2,5 +2,5 @@ require "./xlsx-parser/book"
 require "./xlsx-parser/sheet"
 
 module XlsxParser
-  VERSION = "0.5.1"
+  VERSION = "0.6.0"
 end
