dafde47032e14bcd33b0df7d21202ac2428c9cec
Fabien LEFEBVRE (d1ceward) <contact@d1ceward.com>
2025-08-04 20:01:05 +0200

    Refactor XML parsing to handle missing content and improve error handling

--- src/xlsx-parser/styles/parser.cr
+++ src/xlsx-parser/styles/parser.cr
@@ -3,9 +3,19 @@ module XlsxParser::Styles
     def self.call(book : Book) : Array(Symbol | Nil)
       return [] of Symbol? unless (styles_file = book.zip["xl/styles.xml"]?)
 
-      styles = XML.parse(styles_file.open(&.gets_to_end))
+      styles_content = styles_file.open(&.gets_to_end)
+      return [] of Symbol? unless styles_content
+
+      begin
+        styles = XML.parse(styles_content)
+      rescue ex : Exception
+        # If XML is invalid or cannot be parsed, return empty
+        return [] of Symbol?
+      end
+
       custom_style_types = parse_custom_style_types(styles)
       styles_nodes = styles.xpath_nodes("//*[name()='styleSheet']//*[name()='cellXfs']//*[name()='xf']")
+      return [] of Symbol? unless styles_nodes
 
       styles_nodes.map do |xstyle|
         next unless (num_fmt_id = xstyle.attributes["numFmtId"]?.try(&.content).try(&.to_i))
@@ -17,8 +27,10 @@ module XlsxParser::Styles
     private def self.parse_custom_style_types(styles) : Hash(Int32, Symbol)
       custom_style_types = {} of Int32 => Symbol
 
-      styles.xpath_nodes("//*[name()='styleSheet']//*[name()='numFmts']//*[name()='numFmt']")
-            .each do |xstyle|
+      nodes = styles.xpath_nodes("//*[name()='styleSheet']//*[name()='numFmts']//*[name()='numFmt']")
+      return custom_style_types unless nodes
+
+      nodes.each do |xstyle|
         index_attr = xstyle.attributes["numFmtId"]?
         value_attr = xstyle.attributes["formatCode"]?
         next unless index_attr && value_attr
@@ -33,11 +45,11 @@ module XlsxParser::Styles
     end
 
     private def self.determine_custom_style_type(string : String) : Symbol
-      return :float if string[0] == "_"
-      return :float if string[0] == " 0"
+      return :float if string[0] == '_'
+      return :float if string[0] == '0'
 
       # Looks for one of ymdhis outside of meta-stuff like [Red]
-      return :date_time if string =~ /(^|\])[^\[]*[ymdhis]/i
+      return :date_time if string =~ /(^|\])[^[]*[ymdhis]/i
 
       :unsupported
     end
